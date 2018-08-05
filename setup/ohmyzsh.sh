7a1b2c0d805c60a24a762f88022306f2f0f9bf32
Johann Og <kronolynx@gmail.com>
2018-08-05 20:50:47 +0200

    Remove zsh powerline

--- setup/ohmyzsh.sh
+++ setup/ohmyzsh.sh
@@ -18,7 +18,3 @@ zsh-history-substring-search
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
-if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel9k" ]; then
-	# Powerlevel9k the most awesome Powerline theme for ZSH around!
-	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-fi
