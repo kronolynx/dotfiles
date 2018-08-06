2c7bdc12e3d383f4a9a129b1c7f98f9dcb814d8b
Johann Og <kronolynx@gmail.com>
2018-08-06 19:40:34 +0200

    Update monitor position

--- dotfiles/scripts/monitor.sh
+++ dotfiles/scripts/monitor.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-intern=eDP1
-extern=HDMI1
+hdmi=$(xrandr | grep "HDMI. connected" | cut -f 1 -d " ")
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-	xbacklight = 30
-else
-	xrandr --output "$extern"  --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+case $hdmi in
+    HDMI1)
+        xrandr --output "$hdmi"  --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+        xbacklight = 60
+        ;;
+    HDMI2)
+        xrandr --output "$hdmi"  --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+        xbacklight = 60
+        ;;
+        *)
+        xrandr --output HDMI1 --off --output HDMI2 --off --output eDP1 --auto
+        xbacklight = 30
+esac
 
-	xbacklight = 60
-fi
