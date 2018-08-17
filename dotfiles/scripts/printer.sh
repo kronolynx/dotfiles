f6ec702634bcdc6faf4254f20de3daf3a9fb29f7
Johann Og <kronolynx@gmail.com>
2018-08-17 12:38:42 +0200

    Add troubleshot comment

--- dotfiles/scripts/printer.sh
+++ dotfiles/scripts/printer.sh
@@ -2,6 +2,9 @@
 
 # cups in browser
 # localhost:631
+# if adding printer gives error forbidden:
+# sudo gpasswd --add $USER lp
+# sudo gpasswd --add $USER sys
 
 # for printer address resolution
 sudo systemctl start avahi-daemon.service
