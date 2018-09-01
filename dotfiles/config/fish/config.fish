386e2b759858bd567ff1d8de7502368130806b55
Johann Og <kronolynx@gmail.com>
2018-09-01 19:03:57 +0200

    Update mirrors alias

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -8,19 +8,15 @@ set -gx fish_greeting ''
 # programming
 source ~/.asdf/asdf.fish
 set -gx PATH ~/.cargo/bin $PATH
-#set -gx PATH ~/.local/bin $PATH 
+#set -gx PATH ~/.local/bin $PATH
 
 alias vi=vim
 # search in yay
 alias S="yay -Ss "
 # install with yay
 alias I="yay -Syu "
-#alias swapk="setxkbmap -option caps:swapescape"
-#alias swapk="setxkbmap -modiel pc105 -layout dvorak,es -option grp:alt_shift_toggle,caps:swapescape"
+alias mirrors="sudo pacman-mirrors --fasttrack 15; sudo pacman -Syyu"
 alias swapk="setxkbmap -model pc105 -layout dvorak,es -option grp:alt_shift_toggle,caps:backspace"
 
-alias mirrorupdate="sudo pacman-mirrors -gm rank"
-
-alias v='f -e vim' 
+alias v='f -e vim'
 alias o='a -e xdg-open'
-
