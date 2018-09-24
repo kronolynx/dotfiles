fc2fe473e6911522e0d07014e2672a14ddce75a3
Johann Og <kronolynx@gmail.com>
2018-09-25 00:03:20 +0200

    Add racer

--- setup/302-setup-coding-env.sh
+++ setup/302-setup-coding-env.sh
@@ -12,48 +12,48 @@ add_node() {
             ./install-app.sh yarn
         fi
 
-	if ! command grep -qc '/nvm.sh' "$SHELL_FILE"; then
-		echo "Appending nvm source string to zsh"
+  if ! command grep -qc '/nvm.sh' "$SHELL_FILE"; then
+    echo "Appending nvm source string to zsh"
 
                 command printf "\\n# Set up Node Version Manager
 \\nexport NVM_DIR=\"\$HOME/.nvm\"                            # You can change this if you want.
 \\nexport NVM_SOURCE=\"/usr/share/nvm\"                     # The AUR package installs it to here.
 \\n[ -s \"\$NVM_SOURCE/nvm.sh\" ] && \\. \"\$NVM_SOURCE/nvm.sh\"  # Load NVM" >> "$SHELL_FILE"
-		
-	fi
+
+  fi
         source $SHELL_FILE
         nvm install --lts
 }
 add_rust() {
-	if [ ! -d "$HOME/.cargo" ]; then
-		echo "Installing rust"
-		curl https://sh.rustup.rs -sSf | sh
-		source $HOME/.cargo/env
-	fi
+  if [ ! -d "$HOME/.cargo" ]; then
+    echo "Installing rust"
+    curl https://sh.rustup.rs -sSf | sh
+    source $HOME/.cargo/env
+  fi
 }
 add_rustup(){
-	echo "rustup"
-	rustup self update
-	rustup update
-	rustup component add rust-analysis
-	rustup component add rls-preview
-	rustup component add rust-src
-	rustup toolchain install nightly
-	rustup run nightly cargo install clippy
+  echo "rustup"
+  rustup self update
+  rustup update
+  rustup component add rust-analysis
+  rustup component add rls-preview
+  rustup component add rust-src
+  rustup run nightly cargo install clippy
+  rustup run nightly cargo install racer
 }
 
 add_asdf() {
-		if [ ! -d "$HOME/.asdf" ]; then
-    	echo "asdf"
-    	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
+    if [ ! -d "$HOME/.asdf" ]; then
+      echo "asdf"
+      git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
     fi
     if ! command grep -qc '/asdf.sh' "$SHELL_FILE"; then
-    	echo "Appending asdf source string to zsh"
-    	echo -e '\n# asdf' >> "$SHELL_FILE"
-    	echo -e '. $HOME/.asdf/asdf.sh' >> "$SHELL_FILE"
-    	echo -e '. $HOME/.asdf/completions/asdf.bash' >> "$SHELL_FILE"
-    	source "$SHELL_FILE"
-  	fi
+      echo "Appending asdf source string to zsh"
+      echo -e '\n# asdf' >> "$SHELL_FILE"
+      echo -e '. $HOME/.asdf/asdf.sh' >> "$SHELL_FILE"
+      echo -e '. $HOME/.asdf/completions/asdf.bash' >> "$SHELL_FILE"
+      source "$SHELL_FILE"
+    fi
 }
 
 asdf_plugins() {
@@ -67,12 +67,12 @@ asdf_plugins() {
     #asdf plugin-add redis https://github.com/smashedtoatoms/asdf-redis.git
 }
 add_scala() {
-	./install-app.sh jdk8-openjdk
-	./install-app.sh sbt
-	if [ ! -f "/usr/local/bin/amm" ]; then
-		echo -e "Installing ammonite"
-		sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm'
-	fi
+  ./install-app.sh jdk8-openjdk
+  ./install-app.sh sbt
+  if [ ! -f "/usr/local/bin/amm" ]; then
+    echo -e "Installing ammonite"
+    sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm'
+  fi
 }
 
 add_node
