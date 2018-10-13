d22f7d0817471c3ef4958bbaabfe391f22481b81
Johann Og <kronolynx@gmail.com>
2018-10-13 19:16:52 +0200

    Fix globing and word splitting

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -13,37 +13,37 @@ backup_dir="backup-$(date +'%Y%m%d')$(date +'%H%M%S')"
 
 # backup each file in dotfiles_origin that exist in home
 shopt -s globstar
-for file in $dotfiles_origin/**
+for file in "$dotfiles_origin"/**
 do
-    # check that the file exist
-    if [ -f $file ]; then
-        f=${file#${dotfiles_origin}/}
-        home_file=$HOME/.$f
-        # check that the file exist in home
-        if [ -f $home_file ]; then
-            destination_dir=$backup_dir/$(dirname $f)
-            mkdir -p $destination_dir
-            cp --remove-destination -r $home_file $backup_dir/$f
-        fi
+  # check that the file exist
+  if [ -f "$file" ]; then
+    f="${file#${dotfiles_origin}/}"
+    home_file="$HOME/.$f"
+    # check that the file exist in home
+    if [ -f "$home_file" ]; then
+      destination_dir="$backup_dir/$(dirname "$f")"
+      mkdir -p "$destination_dir"
+      cp "$home_file" "$destination_dir/"
     fi
+  fi
 done
 
 # copy each file from the origin in to home overriding the original one
 files=$(ls $dotfiles_origin)
 for f in ${files[*]}; do
-    if [ -d $dotfiles_origin/$f ]; then
-        mkdir -p $HOME/.$f
-        cp --remove-destination -r $dotfiles_origin/$f/* $HOME/.$f/
-    else
-        file="${f##*/}"
-        cp --remove-destination $dotfiles_origin/$file $HOME/.$file
-    fi
+  if [ -d $dotfiles_origin/$f ]; then
+    mkdir -p $HOME/.$f
+    cp --remove-destination -r $dotfiles_origin/$f/* $HOME/.$f/
+  else
+    file="${f##*/}"
+    cp --remove-destination $dotfiles_origin/$file $HOME/.$file
+  fi
 done
 # make xinit executable
 xinit=~/.xinitrc
 if [ -f $xinit ]; then
   if [ ! -x $xinit ]; then
-     chmod +x $xinit
+    chmod +x $xinit
   fi
   # for ligthdm to run xinitrc
   if [ ! -f ~/.xsession ]; then
