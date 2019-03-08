a50bae81bdd4b2ce856cb4d1299795f5198b774a
Johann Og <kronolynx@gmail.com>
2019-03-08 10:38:30 +0100

    Update fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,33 +1,26 @@
 fish_vi_key_bindings
-
+set fish_plugins autojump vi-mode
+set -gx TERM rxvt-256color
 set -Ux EDITOR "emacsclient -nw -c emacs"
 set -Ux VISUAL "emacsclient -nw -c emacs"
 
-set -gx fish_greeting ''
-
-# programming
-#source ~/.asdf/asdf.fish
-#set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 
 # console emacs
-alias em="emacsclient -nw -c -a ecamcs"
+alias em="emacsclient -nw -c -a emacs"
+alias emc="emacsclient -c -a emacs"
 alias vi=vim
-# # search in yay
-# alias S="yay -Ss "
-# # install with yay
-# alias I="yay -Syu "
-# alias mirrors="sudo pacman-mirrors --fasttrack 15; sudo pacman -Syyu"
-# alias swapk="setxkbmap -model pc105 -layout dvorak,us -option grp:alt_shift_toggle,caps:backspace"
 
 # apt alias
 alias S="apt search "
 alias U="sudo apt update; and sudo apt upgrade"
 alias I="sudo apt install "
 alias R="sudo apt remove "
-
-alias g=git
-alias F="cat ~/.config/fish/config.fish | grep 'alias'"
+function g --wraps git
+  git $argv;
+end
+alias LF="cat ~/.config/fish/config.fish | grep 'alias'"
+alias SF="source ~/.config/fish/config.fish"
 function get_port
     lsof -i ":$argv"
 end
@@ -36,19 +29,27 @@ alias P="get_port " #"lsof -i :" #add port e.g. 8080
 alias tkd="tmux list-sessions | grep -v attached | cut -d: -f1 |  xargs -t -n1 tmux kill-session -t"
 alias tk="tmux kill-session -t "
 
-function mk_cd
-    mkdir -p "$argv"; and cd "$argv"
-end
-alias md=mk_cd
+alias swapk="setxkbmap -model pc105 -layout dvorak,es -option grp:alt_shift_toggle,caps:backspace"
 
+# kill any process listening on the port given e.g: kp 8080
+function kp
+  kill -9 (lsof -t -i:$argv) 2>/dev/null; and echo "Process on port $argv killed" ;or echo "Nothing listening on port $argv"
+end
 alias v='f -e vim'
 alias o='a -e xdg-open'
 alias cat=bat
+alias ls=lsd
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
 alias untar='tar -sxvf '
 alias wget='wget -c '
 alias ipe='curl ipinfo.io/ip'
+# make a directory and cd into it
+function md
+    mkdir -p "$argv"; and cd "$argv"
+end
 
-# bobthefish theme
-set -g theme_nerd_fonts yes
-set -g theme_display_vi yes
-set -g theme_color_scheme dracula
+set -U fish_key_bindings fish_vi_key_bindings
+set -U budspencer_nogreeting
