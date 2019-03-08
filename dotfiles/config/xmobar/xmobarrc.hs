308dbfd1821f66be00d4d9ec235eca1430e9cbf8
Johann Og <kronolynx@gmail.com>
2019-03-08 10:43:57 +0100

    Update xmobar

--- dotfiles/config/xmobar/xmobarrc.hs
+++ dotfiles/config/xmobar/xmobarrc.hs
@@ -1,7 +1,7 @@
 Config {
-      font               = "xft:FantasqueSansMono Nerd Font:pixelsize=19"
+      font               = "xft:FantasqueSansMono Nerd Font:pixelsize=19,Noto Sans CJK SC:pixelsize=18,Noto Sans CJK TC:pixelsize=18,Noto Sans CJK JP:pixelsize=18"
     , bgColor            = "#222222"
-    , alpha              = 0
+    , alpha              = 7
     , fgColor            = "grey"
     , position           = TopW L 92
     , hideOnStart        = False
@@ -11,6 +11,7 @@ Config {
     , persistent         = True
     , alignSep           = "}{"
     , commands =
+      [
         [ Run Battery
             [ "--template", "<acstatus>"
             , "--Low"     , "10"
@@ -26,7 +27,7 @@ Config {
             ] 60
         -- , Run Com "sh" ["-c", "awk '{print $1, $2, $3 }' /proc/loadavg"] "loadavg" 10
         -- , Run Com "sh" ["-c", "mpc | head -n -2 | sed 's,.*/,,' | cut -c-40"] "mpd" 50
-        , Run Date "<fc=#00ff00> %a %b %d </fc><fc=#ee9a00> %H:%M:%S</fc>" "date" 10
+         Run Date "<fc=#00ff00> %a %b %d</fc><fc=#ee9a00> %H:%M:%S</fc>" "date" 10
         , Run Mpris2 "spotify" ["-t", "<artist> - <title>"] 10
         -- keyboard layout indicator
         , Run Kbd            [ ("us(dvorak)" , "<fc=#cc00ff>DV</fc>")
