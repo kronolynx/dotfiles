acd23365ac86995a70960c0be57ce5b901e3a6d0
Johann Og <kronolynx@gmail.com>
2019-03-24 17:50:01 +0100

    Make zsh fish-like

--- setup-ubuntu/apps/ohmyzsh.sh
+++ setup-ubuntu/apps/ohmyzsh.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-apps=(
-# bash replacement
-zsh
-# fish like syntax highlight
-zsh-syntax-highlighting
-)
-
-./install-app.sh ${apps[*]}
+sudo apt install -y zsh
 
 # oh-my-zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
+
+if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]; then
+    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search" ]; then
+    git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel9k" ]; then
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/up" ]; then
+    git clone https://github.com/peterhurford/up.zsh ~/.oh-my-zsh/custom/plugins/up
+fi
+
+
