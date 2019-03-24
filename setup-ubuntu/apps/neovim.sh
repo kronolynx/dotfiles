576aace91468b0a464cd30046ce310bd58f787f5
Johann Og <kronolynx@gmail.com>
2019-03-24 18:58:32 +0100

    Set neovim

--- setup-ubuntu/apps/neovim.sh
+++ setup-ubuntu/apps/neovim.sh
@@ -5,9 +5,10 @@ sudo apt install neovim -y
 
 mkdir -p ~/.config/nvim
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # import existing configuration
 # ln -s ~/.vimrc ~/.config/nvim/init.vim
 # or create one from current
- cp ~/.vimrc ~/.config/nvim/init.vim
+# cp ~/.vimrc ~/.config/nvim/init.vim
+
+ mkdir -p ~/.vim/undo
+ mkdir -p ~/.vim/backup
