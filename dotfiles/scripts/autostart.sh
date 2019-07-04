ae48bcd29fb5401e31f1152c9e6a1f45395046cf
Johann Og <kronolynx@gmail.com>
2019-07-04 09:09:18 +0200

    Add trayer to autostart

--- dotfiles/scripts/autostart.sh
+++ dotfiles/scripts/autostart.sh
@@ -6,8 +6,10 @@ function run {
     $@&
   fi
 }
-
-#(sleep 2; run $HOME/.config/polybar/launch.sh) &
+if ! pgrep "trayer" > /dev/null
+then
+  trayer --edge top --align right --widthtype request --expand true --SetDockType true --SetPartialStrut false --transparent true --alpha 0 --tint 0x2F343F --expand true --heighttype pixel --height 24 --monitor 0 --padding 1 --distance 4 --distancefrom top &
+fi
 compton --config $HOME/.config/compton/compton.conf &
 run compton &
 run xfce4-power-manager &
