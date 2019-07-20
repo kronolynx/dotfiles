8fc14dcc306571c90094d554b5ad1c07b27332af
Johann Og <kronolynx@gmail.com>
2019-07-20 20:28:32 +0200

    Misc changes, remove unused apps

--- dotfiles/scripts/autostart.sh
+++ dotfiles/scripts/autostart.sh
@@ -11,7 +11,10 @@ then
   trayer --edge top --align right --widthtype request --expand true --SetDockType true --SetPartialStrut false --transparent true --alpha 0 --tint 0x2F343F --expand true --heighttype pixel --height 24 --monitor 0 --padding 1 --distance 4 --distancefrom top &
 fi
 compton --config $HOME/.config/compton/compton.conf &
+
+# set x cursor
+# https://wiki.haskell.org/Xmonad/Frequently_asked_questions#Setting_the_X_cursor
+xsetroot -cursor_name left_ptr
 run compton &
 run xfce4-power-manager &
-run clipit &
 run dropbox &
