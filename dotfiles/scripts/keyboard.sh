8fb4542683f477850de92a919a10843c8e252b50
Johann Og <kronolynx@gmail.com>
2019-07-31 11:39:40 +0200

    Update zsh alias, keyboard script

--- dotfiles/scripts/keyboard.sh
+++ dotfiles/scripts/keyboard.sh
@@ -9,5 +9,5 @@ setxkbmap -model pc105 -layout dvorak,us
 # lv3:ralt_switch_multikey -> Right Alt; Shift+Right Alt as Compose
 # https://wiki.archlinux.org/index.php/Xorg/Keyboard_configuration#Configuring_compose_key
 setxkbmap -option # remove previous options
-setxkbmap -option caps:backspace,shift:both_capslock,grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch,compose:rctrl,eurosign:4
+setxkbmap -option caps:backspace,shift:both_capslock,grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch,compose:menu,eurosign:4
 xmodmap -e "clear Lock"
