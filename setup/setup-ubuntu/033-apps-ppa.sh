4f3af77d0e374da9ce2c5345f0ce4250e535a543
Johann Og <kronolynx@gmail.com>
2019-09-29 12:15:25 +0200

    Update ppa script

--- setup/setup-ubuntu/033-apps-ppa.sh
+++ setup/setup-ubuntu/033-apps-ppa.sh
@@ -2,93 +2,27 @@
 
 SCRIPTPATH="$(dirname $(realpath $0))"  # script location directory to fix relative path calls
 
-add_ppas() {
-  # add ppas after 
-  sudo add-apt-repository ppa:tista/adapta -y # theme adapta-nokoto
-  sudo add-apt-repository ppa:noobslab/icons -y # icons
-  sudo apt-add-repository ppa:numix/ppa -y # numix icons
-  sudo add-apt-repository ppa:system76/pop -y  # pop theme
-  sudo apt install pop-theme sudo add-apt-repository ppa:papirus/papirus -y # papirus icons
-}
-
-accessories=(
-
+declare -A ppas=(
+  # usage
+  # ["ppaName"]="appName"
+  ["danielrichter2007/grub-customizer"]="grub-customizer"
 )
 
-development=(
+$SCRIPTPATH/helpers/pprint.sh "PPA script" "green"
 
-)
-
-education=(
-
-)
-
-graphics=(
-
-)
-
-internet=(
-
-)
-
-multimedia=(
-
-)
-
-
-office=(
-
-)
-
-other=(
-
-)
-
-themes=(
-  arc-theme
-  adapta-gtk-theme
-  obsidian-1-icons
-  shadow-icon-theme
-  dalisha-icons
-  papirus-icon-theme
-  breeze-cursor-theme
-  pop-theme
-)
-
-system=(
-  
-)
-
-
-add_ppas
-sudo apt update
-
-$SCRIPTPATH/helpers/pprint.sh "accessories" 
-$SCRIPTPATH/helpers/install-app.sh ${accessories[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "development" 
-$SCRIPTPATH/helpers/install-app.sh ${development[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "education" 
-$SCRIPTPATH/helpers/install-app.sh ${education[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "graphics" 
-$SCRIPTPATH/helpers/install-app.sh ${graphics[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "internet" 
-$SCRIPTPATH/helpers/install-app.sh ${internet[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "multimedia" 
-$SCRIPTPATH/helpers/install-app.sh ${multimedia[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "office" 
-$SCRIPTPATH/helpers/install-app.sh ${office[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "other" 
-$SCRIPTPATH/helpers/install-app.sh ${other[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "themes" 
-$SCRIPTPATH/helpers/install-app.sh ${themes[*]}
-
-$SCRIPTPATH/helpers/pprint.sh "system" 
-$SCRIPTPATH/helpers/install-app.sh ${system[*]}
+for ppa in "${!ppas[@]}";
+do
+  ppa_name=$(cut -d "/" -f1 <<< $ppa)
+  # if ppa already added do nothing
+  if ! ls /etc/apt/sources.list.d/ | grep -q "^$ppa_name.*list"; then
+    $SCRIPTPATH/helpers/pprint.sh "adding ppa $ppa" 
+    sudo add-apt-repository "ppa:$ppa" -y
+    sudo apt update
+  fi
+  package="${ppas[$ppa]}"
+  # if package already installed do nothing
+  if ! dpkg -s $package &> /dev/null; then
+    $SCRIPTPATH/helpers/pprint.sh "installing $package" 
+    sudo apt install "$package" -y
+  fi
+done
