05181f9107ec3ece32a8767c9243a9472ad73b1f
Johann Og <kronolynx@gmail.com>
2019-10-15 13:28:50 +0200

    Sync files with home

--- dotfiles/scripts/autostart_work.sh
+++ dotfiles/scripts/autostart_work.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-./elecom.sh
+~/.scripts/elecom.sh
 
+setxkbmap -model pc105 -layout us,es
+
+setxkbmap -option # remove previous options
+setxkbmap -option grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch,compose:menu,eurosign:4
+xmodmap -e "clear Lock"
--- setup/setup-ubuntu/032-apps-deb.sh
+++ setup/setup-ubuntu/032-apps-deb.sh
@@ -5,7 +5,8 @@ SCRIPTPATH="$(dirname $(realpath $0))"  # script location directory to fix relat
 debs=(
   # add here links to direct downloads to .debs to install
   https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
+  https://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_19.04/amd64/albert_0.16.1_amd64.deb
 )
 
 $SCRIPTPATH/helpers/pprint.sh "Installing debs" 
-$SCRIPTPATH/helpers/install_deb.sh ${deb[*]}
+$SCRIPTPATH/helpers/install-deb.sh ${debs[*]}
