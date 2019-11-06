7373bbb1acc517d092922c82cbc9cb992374d84a
Johann Og <kronolynx@gmail.com>
2019-11-06 15:06:30 +0100

    Refactor emacs config

---
+++ dotfiles/emacs.d/elisp/lang/lang-lsp.el
@@ -0,0 +1,25 @@
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :custom
+  (lsp-auto-guess-root t)
+  (lsp-prefer-flymake nil)
+  :hook
+  ((scala-mode c-mode c++-mode python-mode) . lsp)
+  :config
+  (require 'lsp-clients)
+  )
+
+(use-package lsp-ui)
+
+;; Add company-lsp backend for metals
+(use-package company-lsp
+  :after company
+  )
+
+(use-package dap-mode
+  :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+(provide 'lang-lsp)
