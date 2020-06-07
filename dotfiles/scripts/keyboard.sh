c63f3dfd93b628ddfdd7946fd4dfb343ddf31c27
Johann Og <kronolynx@gmail.com>
2020-06-07 12:31:51 +0200

    Set Shift+RightAlt as compose key

--- dotfiles/scripts/keyboard.sh
+++ dotfiles/scripts/keyboard.sh
@@ -8,7 +8,7 @@ setxkbmap -model pc105 -layout dvorak,us
 # lv3:ralt_switch_multikey -> Right Alt; Shift+Right Alt as Compose
 # https://wiki.archlinux.org/index.php/Xorg/Keyboard_configuration#Configuring_compose_key
 setxkbmap -option # remove previous options
-setxkbmap -option caps:backspace,grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch,compose:menu,eurosign:4
+setxkbmap -option caps:backspace,grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch_multikey,eurosign:4
 xmodmap -e "clear Lock"
 
 fcitx
