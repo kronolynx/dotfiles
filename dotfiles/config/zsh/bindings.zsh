c7373a05c1b07018df765ddf9a15175fedfd7fed
Johann Og <kronolynx@gmail.com>
2020-12-08 12:24:23 +0100

    Back to zsh

--- dotfiles/config/zsh/bindings.zsh
+++ dotfiles/config/zsh/bindings.zsh
@@ -9,8 +9,8 @@ bindkey '^r' history-incremental-search-backward
 # {{{
 
 # substring {{{
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-bindkey '^[[A' history-substring-search-up			
-bindkey '^[[B' history-substring-search-down
+# bindkey "$terminfo[kcuu1]" history-substring-search-up
+# bindkey "$terminfo[kcud1]" history-substring-search-down
+# bindkey '^[[A' history-substring-search-up			
+# bindkey '^[[B' history-substring-search-down
 # }}}
