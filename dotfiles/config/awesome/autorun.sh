fc85892a0662cabf644e9516acb1fff7f5b8a35d
Johann Og <kronolynx@gmail.com>
2020-12-30 14:38:07 +0100

    Update awesomewm autorun

--- dotfiles/config/awesome/autorun.sh
+++ dotfiles/config/awesome/autorun.sh
@@ -3,41 +3,17 @@
 $HOME/.scripts/monitor.sh &
 $HOME/.scripts/keyboard.sh &
 
-if (! pgrep feh); then
-    feh --bg-scale ~/.wallpapers/girl-anime.jpg &
-fi
+start() {
+  [ -z "$(pidof -x $1)" ] && $@ &
+}
 
-if (! pgrep thunar); then
-    thunar --daemon &
-fi
-
-if (! pgrep lxpolkit); then
-    lxpolkit &
-fi
-
-if (! pgrep xfce4-power-management); then
-    xfce4-power-management &   
-fi
-
-if (! pgrep nm-applet); then
-    nm-applet &   
-fi
-
-if (! pgrep pa-applet); then
-    pa-applet &   
-fi
-
-if (! pgrep xfce4-power-manager); then
-    xfce4-power-manager &   
-fi
-
-if (! pgrep picom); then
-    picom -C -b &
-fi
-
-if (! pgrep xautolock); then
-    xautolock -time 7 -corners -000 -locker lock &
-fi
+start feh --bg-scale ~/.wallpapers/no-mans-sky.jpg
+start lxpolkit
+start xfce4-power-management
+start nm-applet
+start pa-applet
+start picom -C -b
+#    xautolock -time 7 -corners -000 -locker lock &
 
 if [ -f ~/.scripts/autostart_work.sh ]; then
   ~/.scripts/autostart_work.sh
