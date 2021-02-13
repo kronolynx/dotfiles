b40f15fa035d05823dc240fb4f1c2858c30db063
Johann Og <kronolynx@gmail.com>
2021-02-13 11:21:59 +0100

    Update zsh

--- dotfiles/config/zsh/dev.zsh
+++ dotfiles/config/zsh/dev.zsh
@@ -6,6 +6,14 @@ alias dpubl="sbt compile publishLocal docker:publishLocal && notify-send Publish
 alias dpublr="ag -l | entr -s 'sbt compile publishLocal docker:publishLocal && notify-send Published dpublr || notify-send Failed dpublr'" # http://eradman.com/entrproject/
 alias cpubl="sbt clean compile publishLocal publishLocal && notify-send Published cpubl || notify-send Failed cpubl"
 alias cdpubl="sbt clean compile publishLocal docker:publishLocal && notify-send Published cdpubl || notify-send Failed cdpubl"
+alias pdom="sbt wr-admin-domain/compile wr-admin-domain/publishLocal"
+alias cpdom="sbt wr-admin-domain/clean wr-admin-domain/compile wr-admin-domain/publishLocal"
+alias dpdom="sbt wr-admin-domain/compile wr-admin-domain/publishLocal wr-admin-domain/docker:publishLocal"
+alias cdpdom="sbt wr-admin-domain/clean wr-admin-domain/compile wr-admin-domain/publishLocal wr-admin-domain/docker:publishLocal"
+alias papi="sbt wr-admin-api/compile wr-admin-api/publishLocal"
+alias cpapi="sbt wr-admin-api/clean wr-admin-api/compile wr-admin-api/publishLocal"
+alias dpapi="sbt wr-admin-api/compile wr-admin-api/publishLocal wr-admin-api/docker:publishLocal"
+alias cdpapi="sbt wr-admin-api/clean wr-admin-api/compile wr-admin-api/publishLocal wr-admin-api/docker:publishLocal"
 alias dw='watch docker ps -a'
 alias dl='docker logs -f '
 alias dla='docker logs -f wr-admin-api-server'
@@ -23,10 +31,11 @@ alias sbt-res="mv ~/.sbt/repositories-temp ~/.sbt/repositories"
 alias ccache="rm -rf /root/.ivy2/cache/"
 alias domr="sbt wr-admin-domain-server/run"
 alias apir="sbt wr-admin-api-server/run"
-alias testi="sbt panic wr-admin-it/compile wr-admin-it/it:testInteractive"
+alias testi="sbt -Dsbt.supershell=false panic wr-admin-it/compile wr-admin-it/it:testInteractive"
 alias testo="sbt wr-admin-it/it:testOnly"
 alias rlds="find src/ -name '*.scala' | entr -s " # requires argument e.g 'sbt test'
 # scala metals log
 alias smlog='tail -f .metals/metals.log'
+alias amm212="cs launch com.lihaoyi:ammonite_2.12.10:2.0.4 -M ammonite.Main --"
 
 # export SBT_OPTS="-Xmx2G -XX:MaxMetaspaceSize=1024m -Dsbt.boot.credentials=$HOME/.sbt/.credentials -Dsbt.override.build.repos=true -Xss2M"
