d805c140eae2677aba11faa452b72bc7fe731ed7
Johann Og <kronolynx@gmail.com>
2021-03-27 10:48:03 +0100

    Remove rsync from backup script

--- scripts/backup_dotfiles.sh
+++ scripts/backup_dotfiles.sh
@@ -9,25 +9,30 @@ echo -e "############## \e[32m Copying dotfiles \e[36m '$DOT_DIR' \e[0m"
 echo -e "################################################################"
 echo -e ""
 
-if ! command -v rsync &> /dev/null
-then
-    echo "Error: rsync could not be found"
-    exit
-fi
-
 if [ -d $DOT_DIR ]; then
     backup="backup-$(date +'%Y%m%d')$(date +'%H%M%S')"
     cp -r $DOT_DIR $backup
+else
+    mkdir -p $DOT_DIR
 fi
 
+copy_files() {
+    for f in "$@" ; do
+        local file_or_dir=$HOME/.$f
+        if [ -f $file_or_dir ] && [ ! -L $file_or_dir ]; then
+            mkdir -p $DOT_DIR/$(dirname $f)
+            echo -e "\e[32mcopying\e[0m  => $f"
+            cp --remove-destination $file_or_dir $DOT_DIR/$f
+        elif [ -f $f ] && [ ! -L $f ]; then
+            next_file=$(echo $f | cut -d "." -f 2-)
+            copy_files $next_file
+        elif [ -d $file_or_dir ]; then
+            next_files=($file_or_dir/*)
+            copy_files $next_files
+        else 
+            echo -e "\e[33mskipping\e[0m => $file_or_dir"
+        fi
+    done
+}
 
-for f in "$@" ; do
-    file_or_dir=$HOME/.$f
-    if [ -f $file_or_dir ]; then
-        mkdir -p $DOT_DIR/$(dirname $f)
-        rsync --no-links $file_or_dir $DOT_DIR/$f
-    elif [ -d $file_or_dir ]; then
-        mkdir -p $DOT_DIR/$f
-        rsync -r --no-links --delete-during $file_or_dir/* $DOT_DIR/$f
-    fi
-done
+copy_files "$@" 
