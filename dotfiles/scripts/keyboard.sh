3ef2df8ccd53568ff53ac31a533afbd621fc21b6
Johann Og <kronolynx@gmail.com>
2021-11-02 21:55:59 +0100

    Update keyboard

--- dotfiles/scripts/keyboard.sh
+++ dotfiles/scripts/keyboard.sh
@@ -8,7 +8,7 @@ setxkbmap -model pc105 -layout us -variant altgr-intl
 # lv3:ralt_switch_multikey -> Right Alt; Shift+Right Alt as Compose
 # https://wiki.archlinux.org/index.php/Xorg/Keyboard_configuration#Configuring_compose_key
 setxkbmap -option # remove previous options
-setxkbmap -option caps:backspace,grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch_multikey,eurosign:4
+setxkbmap -option caps:backspace,grp:alt_space_toggle,terminate:ctrl_alt_bksp,lv3:ralt_switch_multikey,eurosign:4,shift:both_capslock_cancel
 xmodmap -e "clear Lock"
 
 synclient TapButton1=1 TapButton2=3 TapButton3=2
---
+++ setup/common/apps/asdf.sh
@@ -0,0 +1,5 @@
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+# asdf list all nodejs
+
+asdf plugin add rust https://github.com/code-lever/asdf-rust
+asdf install rust latest
