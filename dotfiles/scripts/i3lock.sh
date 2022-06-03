ec25b735093eae70d17b00329359393a5f959773
Johann Og <kronolynx@gmail.com>
2022-06-03 14:38:12 +0200

    Update logout menu

--- dotfiles/scripts/i3lock.sh
+++ dotfiles/scripts/i3lock.sh
@@ -1,26 +1,50 @@
 #!/bin/sh
 
+case "$DESKTOP_SESSION" in
+    xfce)
+        LOCK=("xflock4")
+        LOGOUT=("xfce4-session-logout" "--logout")
+        SUSPEND=("xfce4-session-logout" "--suspend")
+        HiBERNATE=("xfce4-session-logout" "--hibernate")
+        REBOOT=("xfce4-session-logout" "--reboot")
+        SHUTDOWN=("xfce4-session-logout" "--halt")
+        ;;
+    xmonad*)
+        LOCK=("~/.local/bin/lock")
+        LOGOUT=("pkill" "-u" "$USER")
+        SUSPEND=("~/.local/bin/lock" "&&" "systemctl" "suspend")
+        HiBERNATE=("lock" "&&" "systemctl" "hibernate")
+        REBOOT=("systemctl" "reboot")
+        SHUTDOWN=("systemctl" "poweroff")
+        ;;
+    *)
+        echo "Unknown commands for $DESKTOP_SESSION"
+        exit 2
+        
+esac
+
+
 case "$1" in
     lock)
-        ~/.local/bin/lock
+        "${LOCK[@]}"
         ;;
     screenoff)
         ~/.local/bin/lock && xset dpms force off
         ;;
     logout)
-        i3-msg exit
+        "${LOGOUT[@]}"
         ;;
     suspend)
-        ~/.local/bin/lock && systemctl suspend
+        "${SUSPEND[@]}"
         ;;
     hibernate)
-        lock && systemctl hibernate
+        "${HIBERNATE[@]}"
         ;;
     reboot)
-        systemctl reboot
+        "${REBOOT[@]}"
         ;;
     shutdown)
-        systemctl poweroff
+        "${SHUTDOWN[@]}"
         ;;
     *)
         echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
