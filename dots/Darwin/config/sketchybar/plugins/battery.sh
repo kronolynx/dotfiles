caa12d17f62b84c955364700488bd503ab44d566
Johann Og <kronolynx@gmail.com>
2023-02-27 23:19:37 +0100

    Add basic sketchybar config

--- dots/Darwin/config/sketchybar/plugins/battery.sh
+++ dots/Darwin/config/sketchybar/plugins/battery.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+source "$HOME/.config/sketchybar/icons.sh"
+source "$HOME/.config/sketchybar/colors.sh"
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
@@ -7,22 +10,23 @@ if [ $PERCENTAGE = "" ]; then
   exit 0
 fi
 
+DRAWING=on
+COLOR=$WHITE
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON=$BATTERY_100;
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON=$BATTERY_75; 
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON=$BATTERY_50
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
   ;;
-  *) ICON=""
+  *) ICON=$BATTERY_0; COLOR=$RED
 esac
 
 if [[ $CHARGING != "" ]]; then
-  ICON=""
+  ICON=$BATTERY_CHARGING
+  DRAWING=off
 fi
 
-# The item invoking this script (name $NAME) will get its icon and label
-# updated with the current battery status
-sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set $NAME icon="$ICON" icon.color=$COLOR
