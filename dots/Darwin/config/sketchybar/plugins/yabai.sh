f2da8448625df1faa4984a43823a4b80f2a16556
Johann Og <kronolynx@gmail.com>
2023-03-28 10:20:47 +0200

    Update sketchybar space icons

--- dots/Darwin/config/sketchybar/plugins/yabai.sh
+++ dots/Darwin/config/sketchybar/plugins/yabai.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-window_state() {
-  source "$HOME/.config/sketchybar/colors.sh"
-  source "$HOME/.config/sketchybar/icons.sh"
-
-  WINDOW=$(yabai -m query --windows --window)
-  CURRENT=$(echo "$WINDOW" | jq '.["stack-index"]')
-
-  args=()
-  if [[ $CURRENT -gt 0 ]]; then
-    LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
-    args+=(--set $NAME icon=$YABAI_STACK icon.color=$RED label.drawing=on drawing=on label=$(printf "[%s/%s]" "$CURRENT" "$LAST")
-          --bar border_color=$RED)
-  else
-    args+=(--set $NAME label.drawing=off drawing=off)
-    case "$(echo "$WINDOW" | jq '.["is-floating"]')" in
-      "false")
-        if [ "$(echo "$WINDOW" | jq '.["has-fullscreen-zoom"]')" = "true" ]; then
-          ICON=$YABAI_FULLSCREEN_ZOOM
-          COLOR=$GREEN
-        elif [ "$(echo "$WINDOW" | jq '.["has-parent-zoom"]')" = "true" ]; then
-          ICON=$YABAI_PARENT_ZOOM
-          COLOR=$BLUE
-        else
-          ICON=$YABAI_GRID
-          COLOR=$BAR_BORDER_COLOR
-        fi
-        ;;
-      "true")
-        ICON=$YABAI_FLOAT
-        COLOR=$MAGENTA
-        ;;
-    esac
-    args+=(--animate sin 10 --bar border_color=$COLOR)
-  fi
-
-  sketchybar -m "${args[@]}"
-}
 
 windows_on_spaces () {
   CURRENT_SPACES="$(yabai -m query --displays | jq -r '.[].spaces | @sh')"
@@ -46,11 +9,11 @@ windows_on_spaces () {
     for space in $line
     do
       icon_strip=" "
-      apps=$(yabai -m query --windows --space $space | jq -r ".[].app")
+      apps=$(yabai -m query --windows --space $space | jq -r '.[] | {app, "is-sticky"} | join (" ")')
       if [ "$apps" != "" ]; then
-        while IFS= read -r app; do
+        while IFS=" " read -r app sticky; do
           app_icon=" $($HOME/.config/sketchybar/plugins/icon_map.sh "$app")"
-          if [[ "$icon_strip" != *"$app_icon"* ]]; then
+          if [[ "$icon_strip" != *"$app_icon"* ]] && [ "$sticky" == false ]; then
             icon_strip+="$app_icon"
           fi
         done <<< "$apps"
@@ -71,13 +34,8 @@ mouse_clicked() {
 }
 
 case "$SENDER" in
-  "mouse.clicked") mouse_clicked
-  ;;
   "forced") exit 0
   ;;
-  #"window_focus")
-  #  window_state
-  #;;
   "windows_on_spaces") windows_on_spaces
   ;;
 esac
