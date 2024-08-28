e79da4632769b69db0f6a0c178d58f9a035670dd
Johann Og <kronolynx@gmail.com>
2024-08-28 13:48:44 +0200

    Update sketchybar space

--- dots/Darwin/config/sketchybar/plugins/space.sh
+++ dots/Darwin/config/sketchybar/plugins/space.sh
@@ -3,13 +3,12 @@
 update() {
   source "$HOME/.config/sketchybar/colors.sh"
   
+  sketchybar --set space.$AEROSPACE_FOCUSED_WORKSPACE  background.border_color=$ICON_COLOR background.color=$ICON_COLOR icon.color=$ICON_HIGHLIGHT_COLOR display=1
   if [ "$(aerospace list-windows --workspace $AEROSPACE_PREV_WORKSPACE)" != "" ]; then
     sketchybar --set space.$AEROSPACE_PREV_WORKSPACE  background.border_color=$BACKGROUND_SPACE background.color=$BACKGROUND_SPACE icon.color=$ICON_COLOR
   else
     sketchybar --set space.$AEROSPACE_PREV_WORKSPACE  background.border_color=$BACKGROUND_SPACE background.color=$BACKGROUND_SPACE icon.color=$ICON_COLOR display=0
   fi
-
-  sketchybar --set space.$AEROSPACE_FOCUSED_WORKSPACE  background.border_color=$ICON_COLOR background.color=$ICON_COLOR icon.color=$ICON_HIGHLIGHT_COLOR display=1
 }
 
 if [ "$SENDER" == "aerospace_workspace_change" ]; then
